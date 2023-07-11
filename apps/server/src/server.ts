66b1668c4d03ecc55d6828472cc421ee51bb0fa5
Kevin Do <do.kevin.vo@gmail.com>
2023-07-11 01:53:51 -0700

    :sparkles: UserController and user.routes handle CRUD ops for User

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,71 +1,53 @@
-import express from "express";
-import { PrismaClient } from "../prisma/src/generated/client";
 import chalk from "chalk";
+import cors from "cors";
+import express from "express";
+// import { PrismaClient } from "../prisma/src/generated/client";
+import { router } from "src/routes";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/feed", async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true },
-  });
-  res.json(posts);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.post("/post", async (req, res) => {
-  const { title, content, authorEmail } = req.body;
-  const post = await prisma.post.create({
-    data: {
-      title,
-      type: "public",
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
-      upvote: 1,
-      downvote: 0,
-    },
-  });
-  res.json(post);
-});
+// app.get("/feed", async (req, res) => {
+//   const posts = await prisma.post.findMany({
+//     where: { published: true },
+//     include: { author: true },
+//   });
+//   res.json(posts);
+// });
 
-app.put("/publish/:id", async (req, res) => {
-  const { id } = req.params;
-  const post = await prisma.post.update({
-    where: { id },
-    data: { published: true },
-  });
-  res.json(post);
-});
+// app.post("/post", async (req, res) => {
+//   const { title, content, authorEmail } = req.body;
+//   const post = await prisma.post.create({
+//     data: {
+//       title,
+//       type: "public",
+//       content,
+//       published: false,
+//       author: { connect: { email: authorEmail } },
+//       upvote: 1,
+//       downvote: 0,
+//     },
+//   });
+//   res.json(post);
+// });
 
-app.post("/user", async (req, res) => {
-  const { first_name, middle_name, last_name, email, role } = req.body;
+// app.put("/publish/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const post = await prisma.post.update({
+//     where: { id },
+//     data: { published: true },
+//   });
+//   res.json(post);
+// });
 
-  const newUser = await prisma.user.create({
-    data: {
-      first_name,
-      last_name,
-      middle_name,
-      email,
-      role,
-    },
-  });
-
-  res.json(newUser);
-});
-
-app.delete("/user/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await prisma.user.delete({
-    where: {
-      id,
-    },
-  });
-  res.json(user);
-});
+app.use(router);
 
 const server = app.listen(PORT, () => {
   console.log(chalk.green(`http://locahost:${PORT}`));
---
+++ apps/server/src/services/prisma.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "prisma/src/generated/client";
+
+export const prisma = new PrismaClient();
